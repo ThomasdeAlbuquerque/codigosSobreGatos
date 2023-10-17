cb8f8d4319afc13f5722b00b07260640da478833
TomatePelado <89364579+ThomasdeAlbuquerque@users.noreply.github.com>
2023-10-17 00:42:08 +0000

    codigo

---
+++ main.js
@@ -0,0 +1,11 @@
+function olaMundo10 (limite){
+    console.log("oláMundo");
+   let olaMundo = "oláMundo"; 
+  if (limite >= 0 ){
+let fim = limite -1;
+limite = fim
+olaMundo10(limite);  
+}1
+}
+olaMundo10(10);
+
